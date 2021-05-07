0dd2ed37edb5fdb6b331a6d8587a3d93afd84692
Savannah Kolb <savannahnkolb@gmail.com>
2021-05-07 16:11:44 -0400

    started click event in js

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
-var requestUrl = 'http://api.openweathermap.org/data/2.5/weather?q=London&appid=43085cc5c122e71c89a6e73c44f6936f'
-var oneCall = `https://api.openweathermap.org/data/2.5/onecall?lat=`+lat+`&lon=`+lon+`&exclude=hourly,daily&appid=43085cc5c122e71c89a6e73c44f6936f`
+var currentCall;
+var fiveDayCall;
+var searchBtn = $("#searchBtn")
+var SearchInput;
 var lat;
 var lon;
 var res;
-// var responseText = document.getElementById('response-text')
+
+// Forloop for putting data onto the HTML
+for (var i = 0; i < localstorage.length; index++) {
+   var city =localStorage.getitem(i);    
+
+   var cityName = $("#cityResults").addClass("list-group-item");
+
+   cityName.append("<li>" + city + "</li>");
+}
+
+var keyCount = 0;
+
+searchBtn.click(function () {
+    searchInput = $("#searchCity").val();
+    currentCall = 'http://api.openweathermap.org/data/2.5/weather?q=London&appid=43085cc5c122e71c89a6e73c44f6936f'
+    fiveDayCall = `https://api.openweathermap.org/data/2.5/onecall?lat=`+lat+`&lon=`+lon+`&exclude=hourly,daily&appid=43085cc5c122e71c89a6e73c44f6936f`
+    
+})
 
 function getAPI(URL) {
     fetch(URL)
@@ -17,7 +36,7 @@ function getAPI(URL) {
     })
 }
 
-getAPI (requestUrl);
+getAPI (currentCall);
 
 function oneCall(URL) {
     fetch(URL)
