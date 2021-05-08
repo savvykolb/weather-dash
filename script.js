b177cff68da8e9eb8e33f6fd696dbf3f498bc2a0
Savannah Kolb <savannahnkolb@gmail.com>
2021-05-07 21:28:16 -0400

    working on click events in search bar

--- script.js
+++ script.js
@@ -12,5 +12,45 @@ var weatherContent = $('#weatherContent');
 
 var APIkey = "43085cc5c122e71c89a6e73c44f6936f";
 
+//acccess data
 var cityList = [];
+
+//current date and time taken from moment.js
+var currentDate = moment().format('L');
+$("#current-date").text("(" + currentDate + ")");
+
+//This makes sure the page checks history upon page loading
+initalizeHistory();
+showClear();
+
+//I think this can be in an if then statement
+
+//user input taken and added to search history when pressing ENTER
+$(document).on("submit", function(){
+    event.preventDefault();
+
+    var searchValue = searchCityInput.val().trim();
+    currentConditionsRequest(searchValue)
+    searchHistory(searchValue);
+    searchCityInput.val(""); 
+});
+
+//user input taken and added to search history btn click
+searchCityButton.on("click", function(event){
+    event.preventDefault();
+
+    var searchValue = searchCityInput.val().trim();
+
+    currentConditionsRequest(searchValue)
+    searchHistory(searchValue);    
+    searchCityInput.val(""); 
+});
+
+// Clear History
+clearHistoryButton.on("click", function(){
+    cityList = [];
+    listArray();
+    $(this).addClass("hide");
+});
+
 
