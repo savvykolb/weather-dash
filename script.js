8ac2c8b2c4b92c5ec3720be7c4c5d0dcafb70923
Savannah Kolb <savannahnkolb@gmail.com>
2021-05-07 21:43:49 -0400

    current weather working

--- script.js
+++ script.js
@@ -20,37 +20,82 @@ var currentDate = moment().format('L');
 $("#current-date").text("(" + currentDate + ")");
 
 //This makes sure the page checks history upon page loading
-initalizeHistory();
-showClear();
+// initalizeHistory();
+// showClear();
 
-//I think this can be in an if then statement
+//I think this can be in an if then statement to combine both functions
 
 //user input taken and added to search history when pressing ENTER
 $(document).on("submit", function(){
     event.preventDefault();
 
     var searchValue = searchCityInput.val().trim();
-    currentConditionsRequest(searchValue)
+    currentWeather(searchValue)
     searchHistory(searchValue);
     searchCityInput.val(""); 
 });
 
 //user input taken and added to search history btn click
-searchCityButton.on("click", function(event){
+searchButton.on("click", function(event){
     event.preventDefault();
 
     var searchValue = searchCityInput.val().trim();
 
-    currentConditionsRequest(searchValue)
+    currentWeather(searchValue)
     searchHistory(searchValue);    
     searchCityInput.val(""); 
 });
 
 // Clear History
-clearHistoryButton.on("click", function(){
+clearHistoryBtn.on("click", function(){
     cityList = [];
     listArray();
     $(this).addClass("hide");
 });
 
+searchHistoryList.on("click","li.city-btn", function(event) {
+    var value = $(this).data("value");
+    currentConditionsRequest(value);
+    searchHistory(value); 
 
+});
+
+// API request based on user input
+function currentWeather(searchValue) {
+    
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchValue + "&units=imperial&appid=" + APIkey;
+    
+    //This was my fetch functions but switched to AJAX
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response){
+        console.log(response);
+        currentCity.text(response.name);
+        currentCity.append("<small class='text-muted' id='current-date'>");
+        $("#current-date").text("(" + currentDate + ")");
+        currentCity.append("<img src='https://openweathermap.org/img/w/" + response.weather[0].icon + ".png' alt='" + response.weather[0].main + "' />" )
+        currentTemp.text(response.main.temp);
+        currentTemp.append("&deg;F");
+        currentHumidity.text(response.main.humidity + "%");
+        currentWind.text(response.wind.speed + "MPH");
+
+        var lat = response.coord.lat;
+        var lon = response.coord.lon;
+        
+
+        var UVurl = "https://api.openweathermap.org/data/2.5/uvi?&lat=" + lat + "&lon=" + lon + "&appid=" + APIkey;
+        
+        $.ajax({
+            url: UVurl,
+            method: "GET"
+        }).then(function(response){
+            // console.log("UV call: ")
+            // console.log(response);
+            UVindex.text(response.value);
+        });
+
+        var countryCode = response.sys.country;
+        var forecastURL = "https://api.openweathermap.org/data/2.5/forecast?&units=imperial&appid=" + APIkey + "&lat=" + lat +  "&lon=" + lon;
+        
+       
