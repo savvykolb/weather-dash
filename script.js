6046bb226e9ab04ac87778d2a1e39b28297707de
Savannah Kolb <savannahnkolb@gmail.com>
2021-05-07 21:15:04 -0400

    set up global variables

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+//setting up global variables
+var searchHistoryList = $('#searchHistory');
+var searchCityInput = $('#searchCity');
+var searchButton = $('#searchBtn');
+var clearHistoryBtn = $('#clearHistory');
+var currentCity = $('#currentCity');
+var currentTemp = $('#currentTemp');
+var currentHumidity = $('#currentHumidity')
+var currentWind = $('#currentWind');
+var UVindex = $('#uv');
+var weatherContent = $('#weatherContent');
 
 var APIkey = "43085cc5c122e71c89a6e73c44f6936f";
+
+var cityList = [];
 
